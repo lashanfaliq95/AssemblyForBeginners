8bf4ab54a027b3af63ea13deb5a4dfe66ed09c35
lashanfaliq95 <lashanfaliq@gmail.com>
2017-02-06 23:36:44 +0530

    finalised

---
+++ ex4.s
@@ -0,0 +1,56 @@
+@ ARM Assembly - exercise 4
+@ 
+@ Roshan Ragel - roshanr@pdn.ac.lk
+@ Hasindu Gamaarachchi - hasindu@ce.pdn.ac.lk
+
+	.text 	@ instruction memory
+	.global main
+main:
+	@ stack handling, will discuss later
+	@ push (store) lr to the stack
+	sub sp, sp, #4
+	str lr, [sp, #0]
+
+	@ load values
+	@ a,b,i,j in r0,r1,r2,r3 respectively
+	mov r0, #3
+	mov r1, #5
+	mov r2, #7
+	mov r3, #-8
+
+	
+	@ Write YOUR CODE HERE
+	@ if (i>5) f = 70;
+	@ else if (i>3) f=55;
+	@ else f = 30;
+	@ i  in r0
+	@ Put f to r5
+	@ Hint : Use MOV instruction
+	@ MOV r5,#70 makes r5=70
+
+	@ ---------------------
+	
+
+	
+	
+	
+	
+	
+	
+	
+	@ ---------------------
+	
+	
+	@ load aguments and print
+	ldr r0, =format
+	mov r1, r5
+	bl printf
+
+	@ stack handling (pop lr from the stack) and return
+	ldr lr, [sp, #0]
+	add sp, sp, #4
+	mov pc, lr
+
+	.data	@ data memory
+format: .asciz "The Answer is %d (Expect 30 if correct)\n"
+
